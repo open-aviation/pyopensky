05ed407c264121802568b88c2d73ee10f4866164
Xavier Olive <git@xoolive.org>
2024-11-10 16:01:40 +0100

    fix for tzdata missing in windows

--- src/pyopensky/__init__.py
+++ src/pyopensky/__init__.py
@@ -1,3 +1,13 @@
+import sys
 from importlib.metadata import version
 
+from .tzdata import download_tzdata_windows
+
 __version__ = version("pyopensky")
+
+# Despite the 'win32' string, this value is returned on all versions of Windows,
+# including both 32-bit and 64-bit. The 'win32' identifier is a historical
+# artifact that has been retained for compatibility with older versions of
+# Python.
+if sys.platform == "win32":
+    download_tzdata_windows(year=2022)
