dccb662f8b3c96074ba8c657706f33591d513e71
Xavier Olive <git@xoolive.org>
2024-11-10 16:42:13 +0100

    typo in test function

--- tests/test_tzdata.py
+++ tests/test_tzdata.py
@@ -8,6 +8,6 @@ from pyopensky.trino import Trino  # noqa: F401
 
 
 @pytest.mark.skipif(sys.platform != "win32", reason="Only relevant for Windows")
-def check_tzdata() -> None:
+def test_tzdata() -> None:
     folder = Path(os.path.expanduser("~")) / "Downloads" / "tzdata"
     assert (folder / "windowsZones.xml").exists()
